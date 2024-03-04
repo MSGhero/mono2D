424eea97d16911b5f4a827143c523d1c7a8373ba
MSGhero <nj2cool4u@yahoo.com>
2024-03-04 19:37:15 +0100

    Cleaner ctx creation

--- mono/context/ConsumerSystem.hx
+++ mono/context/ConsumerSystem.hx
@@ -20,6 +20,9 @@ class ConsumerSystem extends System {
 	public function new(ecs:Universe) {
 		super(ecs);
 		
+		ecs.setResources(
+			new context.Context(), // custom collection of variables accessible by consumers and specific callbacks
+		);
 	}
 	
 	override function onEnabled() {
